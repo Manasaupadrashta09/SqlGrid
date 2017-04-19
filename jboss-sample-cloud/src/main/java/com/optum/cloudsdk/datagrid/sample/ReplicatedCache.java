061a4086d9679bad88e263b6a2670fd206441c78
mupadras <mupadras@LH7U05CG63035DZ.ms.ds.uhc.com>
2017-04-19 15:53:20 -0500

    commit1

--- jboss-sample-cloud/src/main/java/com/optum/cloudsdk/datagrid/sample/ReplicatedCache.java
+++ jboss-sample-cloud/src/main/java/com/optum/cloudsdk/datagrid/sample/ReplicatedCache.java
@@ -13,6 +13,7 @@ import org.infinispan.client.hotrod.configuration.NearCacheMode;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.context.Flag;
 import org.infinispan.manager.DefaultCacheManager;
+import org.jboss.infinispan.demo.Config.DataGridConfigurationException;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 
 import java.util.UUID;
@@ -27,6 +28,9 @@ import com.optum.cloudsdk.datagrid.sample.DistributedCache;
  * @author
  */
 public class ReplicatedCache {
+	private static final String ENV_VAR_JDG_SERVICE_NAME = "GRID_SAMPLE";
+	private static final String ENV_VAR_SUFFIX_HOTROD_SERVICE_PORT = "_HOTROD_SERVICE_PORT";
+	private static final String ENV_VAR_SUFFIX_HOTROD_SERVICE_HOST = "_HOTROD_SERVICE_HOST";
 
     public RemoteCacheManager getCacheManager() throws Exception {
     	org.infinispan.client.hotrod.configuration.ConfigurationBuilder builder = new org.infinispan.client.hotrod.configuration.ConfigurationBuilder();
@@ -35,10 +39,34 @@ public class ReplicatedCache {
         builder.nearCache()
         .mode(NearCacheMode.LAZY)
         .maxEntries(500).addServer()
-        .host("10.1.26.8")
+        .host("10.1.35.23")
         .port(11222);
         return new RemoteCacheManager(builder.build(), true);
     }
+    
+    private static String getHotRodHostFromEnvironment() throws DataGridConfigurationException {
+		String hotrodServiceName = System.getenv(ENV_VAR_JDG_SERVICE_NAME);
+		if(hotrodServiceName == null) {
+			throw new DataGridConfigurationException("Failed to get JDG Service Name from environment variables. please make sure that you set this value before starting the container");
+		}
+		String hotRodHost=System.getenv(hotrodServiceName.toUpperCase() + ENV_VAR_SUFFIX_HOTROD_SERVICE_HOST);
+		if(hotRodHost == null) {
+			throw new DataGridConfigurationException(String.format("Failed to get hostname/ip address for service %s",hotrodServiceName));
+		}
+		return hotRodHost;
+	}
+	
+	private static int getHotRodPortFromEnvironment() throws DataGridConfigurationException {
+		String hotrodServiceName = System.getenv(ENV_VAR_JDG_SERVICE_NAME);
+		if(hotrodServiceName == null) {
+			throw new DataGridConfigurationException("Failed to get JDG Service Name from environment variables. please make sure that you set this value before starting the container");
+		}
+		String hotRodPort=System.getenv(hotrodServiceName.toUpperCase() + ENV_VAR_SUFFIX_HOTROD_SERVICE_PORT);
+		if(hotRodPort == null) {
+			throw new DataGridConfigurationException(String.format("Failed to get Hot Rod Port for service %s",hotrodServiceName));
+		}
+		return Integer.parseInt(hotRodPort);
+	}
 
     public void saveData(String inputKey, String inputValue) {
         // Setup up a clustered cache manager
@@ -57,7 +85,7 @@ public class ReplicatedCache {
         RemoteCache<String, String> cache = cacheManager.getCache();
         // Store the current node address in some random keys. 
 
-        cache.put(inputKey, inputValue);
+        cache.put("Manasa", "Upadrashta");
 
         cacheManager.stop();
     }
