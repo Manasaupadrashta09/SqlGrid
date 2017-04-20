f38f03b413ae89e5332c8ec54764d141a9353b8c
mupadras <mupadras@LH7U05CG63035DZ.ms.ds.uhc.com>
2017-04-20 14:47:40 -0500

    Final Commit

--- jboss-sample-cloud/src/main/java/com/optum/cloudsdk/datagrid/sample/ReplicatedCache.java
+++ jboss-sample-cloud/src/main/java/com/optum/cloudsdk/datagrid/sample/ReplicatedCache.java
@@ -13,12 +13,13 @@ import org.infinispan.client.hotrod.configuration.NearCacheMode;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.context.Flag;
 import org.infinispan.manager.DefaultCacheManager;
-import org.jboss.infinispan.demo.Config.DataGridConfigurationException;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 
 import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+/*import java.util.logging.Level;
+import java.util.logging.Logger;*/
+import org.apache.log4j.Logger;
+
 
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
@@ -31,43 +32,26 @@ public class ReplicatedCache {
 	private static final String ENV_VAR_JDG_SERVICE_NAME = "JDG_SERVICE_NAME";
 	private static final String ENV_VAR_SUFFIX_HOTROD_SERVICE_PORT = "_HOTROD_SERVICE_PORT";
 	private static final String ENV_VAR_SUFFIX_HOTROD_SERVICE_HOST = "_HOTROD_SERVICE_HOST";
+	final static Logger logger = Logger.getLogger(DistributedCache.class);
 
     public RemoteCacheManager getCacheManager() throws Exception {
     	org.infinispan.client.hotrod.configuration.ConfigurationBuilder builder = new org.infinispan.client.hotrod.configuration.ConfigurationBuilder();
     	
         //Another way to addserver to builder
         //builder.addServer().host("localhost").port(11322);
+        logger.info("*** In the method get Cache Manager ***");
+
         builder.nearCache()
         .mode(NearCacheMode.LAZY)
         .maxEntries(500).addServer()
         .host("10.1.31.4")
-        .port(11222);
+        .port(11333);
+        System.out.println(" ## Just making sure ##");
+
         return new RemoteCacheManager(builder.build(), true);
     }
     
-    private static String getHotRodHostFromEnvironment() throws DataGridConfigurationException {
-		String hotrodServiceName = System.getenv(ENV_VAR_JDG_SERVICE_NAME);
-		if(hotrodServiceName == null) {
-			throw new DataGridConfigurationException("Failed to get JDG Service Name from environment variables. please make sure that you set this value before starting the container");
-		}
-		String hotRodHost=System.getenv(hotrodServiceName.toUpperCase() + ENV_VAR_SUFFIX_HOTROD_SERVICE_HOST);
-		if(hotRodHost == null) {
-			throw new DataGridConfigurationException(String.format("Failed to get hostname/ip address for service %s",hotrodServiceName));
-		}
-		return hotRodHost;
-	}
-	
-	private static int getHotRodPortFromEnvironment() throws DataGridConfigurationException {
-		String hotrodServiceName = System.getenv(ENV_VAR_JDG_SERVICE_NAME);
-		if(hotrodServiceName == null) {
-			throw new DataGridConfigurationException("Failed to get JDG Service Name from environment variables. please make sure that you set this value before starting the container");
-		}
-		String hotRodPort=System.getenv(hotrodServiceName.toUpperCase() + ENV_VAR_SUFFIX_HOTROD_SERVICE_PORT);
-		if(hotRodPort == null) {
-			throw new DataGridConfigurationException(String.format("Failed to get Hot Rod Port for service %s",hotrodServiceName));
-		}
-		return Integer.parseInt(hotRodPort);
-	}
+   
 
     public void saveData(String inputKey, String inputValue) {
         // Setup up a clustered cache manager
@@ -80,13 +64,15 @@ public class ReplicatedCache {
         try {
             cacheManager = getCacheManager();
         } catch (Exception ex) {
-            Logger.getLogger(com.optum.cloudsdk.datagrid.sample.DistributedCache.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(com.optum.cloudsdk.datagrid.sample.DistributedCache.class.getName()).log(Level.SEVERE, null, ex);
+        	logger.error("Error occured:"+ex.getMessage());
+
         }
         // Obtain the default cache
         RemoteCache<String, String> cache = cacheManager.getCache();
         // Store the current node address in some random keys. 
 
-        cache.put("Manasa", "Upadrashta");
+        cache.put("value", "value");
 
         cacheManager.stop();
     }
@@ -96,7 +82,9 @@ public class ReplicatedCache {
         try {
             cacheManager = getCacheManager();
         } catch (Exception ex) {
-            Logger.getLogger(ReplicatedCache.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(ReplicatedCache.class.getName()).log(Level.SEVERE, null, ex);
+        	logger.error("Error occured:"+ex.getMessage());
+
         }
         // Obtain the default cache
         RemoteCache<String, String> cache = cacheManager.getCache(cacheName);
